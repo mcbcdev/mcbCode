03e4ceac785347b6a39ec1e8592971c58a125f24
toast <244019107+xyztoast@users.noreply.github.com>
2026-08-11 15:49:14 -0700

    add obsidian

--- elements/navi.js
+++ elements/navi.js
@@ -20,3 +20,16 @@ fetch("/elements/navi.html")
       }
     });
   });
+
+document.getElementById("get-obsidian-btn").addEventListener("click", async () => {
+    const res = await fetch("https://auth.mcbcode.com/stripe/checkout", {
+        method: "POST",
+        credentials: "include"
+    });
+    const data = await res.json();
+    if (data.ok) {
+        window.location.href = data.checkout_url;
+    } else {
+        alert(data.error || "something went wrong starting checkout");
+    }
+});
